71a7be209cbfa8e362c5d41beb29aa29eaa2f7c6
hamzahl <hamzah_97@hotmail.com>
2022-10-01 13:57:59 +0100

    add intial project setup

---
+++ ui/src/App.tsx
@@ -0,0 +1,11 @@
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+  return (
+    <div class="text-3xl font-bold underline">
+      Site is being developed &#128516;
+    </div>
+  );
+};
+
+export default App;
